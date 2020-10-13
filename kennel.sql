a31ed349084ad60775ee87214440382475c3e58e
Michael Dotson <thedotson@icloud.com>
2020-10-12 21:18:20 -0500

    update

--- kennel.sql
+++ kennel.sql
@@ -1,7 +1,23 @@
 CREATE TABLE `Location` (
 	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	`name`	TEXT NOT NULL,
-	`address`	TEXT NOT NULL
+	`address`	TEXT NOT NULLCREATE TABLE `Animal` (
+    `id`  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name`  TEXT NOT NULL,
+    `status` TEXT NOT NULL,
+    `breed` TEXT NOT NULL,
+    `customer_id` INTEGER NOT NULL,
+    `location_id` INTEGER,
+    FOREIGN KEY(`customer_id`) REFERENCES `Customer`(`id`),
+    FOREIGN KEY(`location_id`) REFERENCES `Location`(`id`)
+  );CREATE TABLE `Employee` (
+    `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    `name`	TEXT NOT NULL,
+    `address`	TEXT NOT NULL,
+    `location_id` INTEGER NOT NULL,
+    FOREIGN KEY(`location_id`) REFERENCES `Location`(`id`)
+  
+  );
 );
 
 CREATE TABLE `Customer` (
