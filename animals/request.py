8204093e62111380e7e2e91d88b785d2271ef770
Michael Dotson <thedotson@icloud.com>
2020-10-19 18:25:18 -0500

    finished CH13

--- animals/request.py
+++ animals/request.py
@@ -103,14 +103,33 @@ def delete_animal(id):
         WHERE id = ?
         """, (id, ))
 
-def update_animal(id, updated_animal):
-# Iterate the ANIMALS list, but use enumerate() so that
-# you can access the index value of each item.
-    for index, animal in enumerate(ANIMALS):
-        if animal.id == id:
-        # Found the animal. Update the value.
-            ANIMALS[index] = Animal(updated_animal['id'], updated_animal['name'], updated_animal['species'], updated_animal['status'], updated_animal['location_id'], updated_animal['customer_id'])
-            break
+def update_animal(id, new_animal):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Animal
+            SET
+                name = ?,
+                breed = ?,
+                status = ?,
+                customer_id = ?,
+                location_id = ?
+        WHERE id = ?
+        """, (new_animal['name'], new_animal['breed'],
+              new_animal['status'], new_animal['customer_id'],
+              new_animal['location_id'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
 
 def get_animals_by_location(location_id):
 
--- employees/request.py
+++ employees/request.py
@@ -75,11 +75,31 @@ def delete_employee(id):
         """, (id, ))
 
 
-def update_employee(id, updated_employee):
-    for index, employee in enumerate(EMPLOYEES):
-        if employee.id == id:
-            EMPLOYEES[index] = Employee(updated_employee['id'], updated_employee['name'], updated_employee['address'], updated_employee['location_id'])
-            break
+def update_employee(id, new_employee):
+    with sqlite3.connect("./kennel.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE Employee
+            SET
+                name = ?,
+                address = ?,
+                location_id = ?
+        WHERE id = ?
+        """, (new_employee['name'], new_employee['address'],
+              new_employee['location_id'], id, ))
+
+        # Were any rows affected?
+        # Did the client send an `id` that exists?
+        rows_affected = db_cursor.rowcount
+
+    if rows_affected == 0:
+        # Forces 404 response by main module
+        return False
+    else:
+        # Forces 204 response by main module
+        return True
+
 
 def get_employees_by_location(location_id):
 
