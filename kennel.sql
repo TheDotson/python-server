d1cfd6d6693982e580ce38579d5e047b6e3d1cad
Michael Dotson <thedotson@icloud.com>
2020-10-20 21:14:54 -0500

    further tweaks

---
+++ kennel.sql
@@ -0,0 +1,10 @@
+        SELECT
+            a.id,
+            a.name,
+            a.address,
+            a.location_id,
+            l.name location_name,
+            l.address location_address
+        FROM employee a
+        JOIN Location l
+            ON l.id = a.location_id
